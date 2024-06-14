31d7ac3eb4f2e3742b5ded3892ea620ba35ebe73
Nguyễn Huy Hoàng <73394906+chopbong@users.noreply.github.com>
2024-06-15 02:17:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const chatBox = document.querySelector(".chatbot");
 
 let userMessage;
 const inputInitHeight = chatInput.scrollHeight;
+const API_KEY = "123456789";
 
 // create a chat <li> element with passed message and className
 const createChatLi = (message, className) => {
@@ -19,8 +20,28 @@ const createChatLi = (message, className) => {
     return chatLi;
 }
 
-const generateReponse = () => {
+const generateReponse = (incomingChatLi) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const messageElement = incomingChatLi.querySelector("p");
+    const requestOptions = {
+        method: "POST",
+        headers : {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body : JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: userMessage}]
+        })
+    }
 
+    // Send POST request to API, get response
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.innerText = data.choices[0].message.content;
+    }).catch(() => {
+        messageElement.classList.add("error");
+        messageElement.innerText = "Opps! Something went wrong. Please try again.";
+    });
 }
 
 const handleChat = () => {
@@ -37,8 +58,9 @@ const handleChat = () => {
     chatBox.scrollTo(0, chatBox.scrollHeight);
 
     setTimeout(() => {
-        chatBox.appendChild(createChatLi("Thinking...", "incoming"));
-        generateReponse();
+        const incomingChatLi = createChatLi("Thinking...", "incoming")
+        chatBox.appendChild(incomingChatLi);
+        generateReponse(incomingChatLi);
         chatBox.scrollTo(0, chatBox.scrollHeight);
     }, 500);
 }
@@ -53,7 +75,7 @@ chatInput.addEventListener("keyup", (event) => {
     if (event.key === "Enter" && !event.shiftKey && window.innerWidth > 800) {
         event.preventDefault();
         handleChat();
-    } 
+    }
 })
 
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
